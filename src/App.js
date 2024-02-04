4282c661ad756997bbb455c73729a94d460d4f13
fenil lad <fenillad087@gmail.com>
2024-02-04 03:53:45 -0500

    updated registration form for label style wth text input

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    
     <div className="container">
       <div className="card divCard">
-      <div>
-       <center><h1 className='title'>Create an Account!!</h1></center>
-      </div>
-      <div>
-        <form>
-          <label for >Email:</label><br/>
-          <input type="text" className='form-control'  id=" floatingEmaillbl" placeholder='Enter  your email'/><br />
+        <div>
+          <center>
+            <h1 className="title">Create an Account!!</h1>
+          </center>
+        </div>
+        <div>
+          <form >
+            <div class="form-floating mb-3">
+              <input type="email " class="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <label for="floatingInput">Email address</label>
+            </div>
 
-          <label>Create Password:</label><br/>
-          <input type="password" className='form-control' placeholder='Create strong password'/><br />
+            <div class="form-floating mb-3">
+                <input type="password" className="form-control" id="floatingPass" placeholder="Create strong password"/>
+                <label for="floatingPass">Create Password:</label>
+            </div>
 
-          <label>Confirm Password:</label><br/>
-          <input type="password"  className='form-control' id='confirmPass' placeholder='Confirm password'/><br />
-
-          <label>Mobile Number:</label><br/>
-          <input type="text"  className='form-control' placeholder='Enter  your mobile number'/><br />
-
-          <label>Shipping address:</label><br/>
-          <input type="text"  className='form-control' placeholder='Enter  your shipping address'/><br />
-
-          <div className='acceptTerms'>
-            <input className='form-check-input' type='checkbox'/>&nbsp;&nbsp;I agree to the terms and conditions of this website.<br/>
-          </div><br></br>
-          <div className='row btn-wrapper'>
-            <button onClick={(e) => alert("Account Created")} className='btn btn-outline-info' id='btnRegister'>Sign Up</button> 
-            <button onClick={(e) => alert("Account Created")} className='btn btn-outline-primary' id='btnLogin'>Sign In</button>
+           <div className="form-floating mb-3"> 
+                <input type="password" className="form-control" id="confirmPass"  placeholder="Confirm password" />
+                <label for="confirmPass">Confirm Password:</label>
+           </div>
+            <div className="form-floating mb-3">
+              <input type="tel" className="form-control" id="lblMobile" placeholder="Enter  your mobile number"/>
+              <label for="lblMobile">Mobile Number:</label>
+            </div>
+            
+            
+          <div className="form-floating mb-3"> <input type="text" className="form-control" id="lblShippingAddress" placeholder="Enter  your shipping address"/>
+            <label for="lblShippingAddress">Shipping address:</label>
           </div>
-          
-        </form>
+           
+          <div className="acceptTerms">
+              <input className="form-check-input" type="checkbox" />
+              &nbsp;&nbsp;I agree to the terms and conditions of this website.
+              <br />
+            </div>
+            <br></br>
+            <div className="row btn-wrapper">
+              <button
+                onClick={(e) => alert("Account Created")}
+                className="btn btn-outline-info"
+                id="btnRegister"
+              >
+                Sign Up
+              </button>
+              <button
+                onClick={(e) => alert("Account Created")}
+                className="btn btn-outline-primary"
+                id="btnLogin"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
