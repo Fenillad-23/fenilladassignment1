a03b545648db8a59a5277bb0669fbda26efdc310
fenil lad <fenillad087@gmail.com>
2024-02-08 18:00:12 -0500

    registration page design updated

--- src/App.js
+++ src/App.js
@@ -10,32 +10,63 @@ function App() {
           </center>
         </div>
         <div>
-          <form >
+          <form>
             <div class="form-floating mb-2">
-              <input type="email " class="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <input
+                type="email "
+                class="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                required
+              />
               <label for="floatingInput">Email address</label>
             </div>
 
             <div class="form-floating mb-2">
-                <input type="password" className="form-control" id="floatingPass" placeholder="Create strong password"/>
-                <label for="floatingPass">Create Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPass"
+                placeholder="Create strong password"
+                required
+              />
+              <label for="floatingPass">Create Password:</label>
             </div>
 
-           <div className="form-floating mb-2"> 
-                <input type="password" className="form-control" id="confirmPass"  placeholder="Confirm password" />
-                <label for="confirmPass">Confirm Password:</label>
-           </div>
             <div className="form-floating mb-2">
-              <input type="tel" className="form-control" id="lblMobile" placeholder="Enter  your mobile number"/>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmPass"
+                placeholder="Confirm password"
+                required
+              />
+              <label for="confirmPass">Confirm Password:</label>
+            </div>
+            <div className="form-floating mb-2">
+              <input
+                type="tel"
+                className="form-control"
+                id="lblMobile"
+                placeholder="Enter  your mobile number"
+                required
+              />
               <label for="lblMobile">Mobile Number:</label>
             </div>
-            
-            
-          <div className="form-floating mb-2"> <input type="text" className="form-control" id="lblShippingAddress" placeholder="Enter  your shipping address"/>
-            <label for="lblShippingAddress">Shipping address:</label>
-          </div>
-           
-          <div className="acceptTerms">
+
+            <div className="form-floating mb-2">
+              {" "}
+              <input
+                type="text"
+                className="form-control"
+                id="lblShippingAddress"
+                placeholder="Enter  your shipping address"
+                required
+              />
+              <label for="lblShippingAddress">Shipping address:</label>
+            </div>
+
+            <div className="acceptTerms">
               <input className="form-check-input" type="checkbox" />
               &nbsp;&nbsp;I agree to the terms and conditions of this website.
               <br />
@@ -43,18 +74,20 @@ function App() {
             <br></br>
             <div className="row btn-wrapper">
               <button
-                onClick={(e) => alert("Account Created")}
-                className="btn btn-outline-info"
+                // onClick={(e) => alert("Account Created")}
+                className="primaryBtn"
                 id="btnRegister"
               >
+                <img src="add-user.png" height={20} />
                 Sign Up
               </button>
+
               <button
                 onClick={(e) => alert("Account Created")}
-                className="btn btn-outline-primary"
-                id="btnLogin"
-              >
-                Sign In
+                className="primaryBtn"
+                id="btnLogin">
+                <img src="login.png" height={20} />
+                &nbsp; Sign In
               </button>
             </div>
           </form>
